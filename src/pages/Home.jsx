b753ec73ccb597a50f453bf7aed65f011ec3882f
ahmad-shk <ahmadsaleem19950@gmail.com>
2024-12-12 23:13:22 +0500

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
-import Form from './Form'
-import Projects from './Projects';
-import Footer from '../components/footer';
+
+
 export default function Home() {
   const primaryColor = '#eb9736';
   const secondaryColor = '#F2F4F1';
